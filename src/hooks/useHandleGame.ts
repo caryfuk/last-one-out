1f4d922c023c1b4890f848491d896ec000847000
Pavel Moravec <pavelm@salsitasoft.com>
2022-12-11 12:13:56 +0100

    generalize rule

--- src/hooks/useHandleGame.ts
+++ src/hooks/useHandleGame.ts
@@ -15,28 +15,11 @@ export const useHandleGame = () => {
     setHumanTurn(Math.random() > 0.5)
   }
 
-  const turn = (items: number, step: number) => {
-    updateDesk(items === step ? Math.floor(3 * Math.random()) + 1 : items - step)
-  }
-
   useEffect(() => {
     const timeout = setTimeout(() => {
-      const items = desk.length
       if (!humanTurn) {
-        if (items >= 17) {
-          turn(items, 17)
-        }
-        else if (items >= 13) {
-          turn(items, 13)
-        }
-        else if (items >= 9) {
-          turn(items, 9)
-        }
-        else if (items >= 5) {
-          turn(items, 5)
-        } else {
-          turn(items, 1)
-        }
+        const toBeRemoved = (desk.length - 1) % 4
+        updateDesk(toBeRemoved === 0 ? Math.floor(3 * Math.random()) + 1 : toBeRemoved)
       }
     }, 1000)
     return () => clearTimeout(timeout)
